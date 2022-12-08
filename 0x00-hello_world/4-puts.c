9872384e29ff12a3dc7f15f55f0cb3cef908b58c
Olubanti <Oluwawolemi.adejugbe@gmail.com>
2022-12-08 13:33:32 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+  Description: "to display a string using the puts method"
+
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
