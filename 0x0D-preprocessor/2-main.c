91bbb99bdb58598f4cac2db6119fd5b0a4c993c2
Olubanti <Oluwawolemi.adejugbe@gmail.com>
2023-01-16 19:12:40 -0800

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,12 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
+#include <stdio.h>
 
-#define ABS(x) ((x < 0) ? (x) * (-1) : (x))
+/**
+ * main - prints the name of the file program was compiled from
+ * Return: Always 0
+ */
 
-#endif
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
